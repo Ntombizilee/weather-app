c1071d1b79681101f0d822d5f4ae841d88d0a47b
Ntombizilee <beingprivy@gmail.com>
2024-10-30 00:27:03 +0200

    Added netlify to my project.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,26 +1,34 @@
-import React from 'react';
+import React from "react";
 
 const Footer = () => {
   return (
     <div>
       <footer>
-          This project was coded by{" "}
-          <a
-            href="https://github.com/Ntombizilee/"
-            target="_blank"
-            rel="noreferrer"
-          >
-            Ntombizile Mabuso
-          </a>{" "}
-          and it is {""}
-          <a
-            href="https://github.com/Ntombizilee/final-react-project"
-            target="_blank"
-            rel="noreferrer"
-          >
-            open-sourced on GitHub
-          </a>
-        </footer>
+        This project was coded by{" "}
+        <a
+          href="https://github.com/Ntombizilee/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Ntombizile Mabuso
+        </a>{" "}
+        and it is {""}
+        <a
+          href="https://github.com/Ntombizilee/final-react-project"
+          target="_blank"
+          rel="noreferrer"
+        >
+          open-sourced on GitHub
+        </a>
+        {""} and{" "}
+        <a
+          href="https://magnificent-starburst-8dd5a3.netlify.app/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          hosted on Netlify{" "}
+        </a>
+      </footer>
     </div>
   );
 };
